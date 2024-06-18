1f8d2720a8af623a860f7a128dd66183ea580bfe
sunbeam-pdac <piyushshinde1902@gmail.com>
2024-06-18 23:20:42 +0530

    Backend API's: Add Property and Get all properties of owner

--- codebase/server/routes/properties.js
+++ codebase/server/routes/properties.js
@@ -14,6 +14,21 @@ router.get("/all/:city", (request, response) => {
   });
 });
 
+// to check
+router.get("/owner/:o_id", (request, response) => {
+  const { o_id } = request.params;
+
+  const statement = `SELECT prop.id, prop.title, prop.address, prop.rate, prop.description, prop.img, p.name, c.category 
+                      FROM properties_tb AS prop
+                      JOIN places_tb AS p ON prop.place_id = p.id 
+                      JOIN categories_tb AS c ON prop.category_id = c.id
+                      WHERE prop.user_id = ?;`;
+
+  db.pool.execute(statement, [o_id], (error, result) => {
+    response.send(utils.createResult(error, result));
+  });
+});
+
 // http://localhost:4000/api/properties/Agra/3
 router.get("/:city/:id", (request, response) => {
   const { city, id } = request.params;
@@ -22,6 +37,30 @@ router.get("/:city/:id", (request, response) => {
   db.pool.execute(statement, [city, id], (error, result) => {
     response.send(utils.createResult(error, result));
   });
+});
+
+// to check
+router.post("/add", (request, response) => {
+  const {
+    title,
+    address,
+    rate,
+    description,
+    img,
+    place_id,
+    category_id,
+    user_id,
+  } = request.body;
+
+  const statement = `INSERT INTO properties_tb (title, address, rate, description, img, place_id, category_id, user_id) VALUES (?,?,?,?,?,?,?,?);`;
+
+  db.pool.execute(
+    statement,
+    [title, address, rate, description, img, place_id, category_id, user_id],
+    (error, result) => {
+      response.send(utils.createResult(error, result));
+    }
+  );
 });
 
 module.exports = router;
